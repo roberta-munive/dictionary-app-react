ca15397e5e13c9a790cfaa5c31d4f6165ead27e8
roberta-munive <114707010+roberta-munive@users.noreply.github.com>
2024-04-16 15:07:48 -0500

    Created component to allow input of the word whose definition will be searched for

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import "./App.css";
+import Search from "./Search";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <div className="container"></div>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <h1 className="text-center">Dictionary</h1>
+        </header>
+        <main>
+          <Search />
+        </main>
+        <footer className="App-footer text-center">
+          This will be the footer
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Search.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import "./Search.css";
+
+export default function Search() {
+  let [word, setWord] = useState(null);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    alert(`Searching for definition of ${word}`);
+  }
+  function updateWord(event) {
+    setWord(event.target.value);
+  }
+
+  return (
+    <div className="Search">
+      <div className="definition-container border rounded">
+        <h2 className="text-center">Look up a word</h2>
+        <form className="definition-form" onSubmit={handleSubmit}>
+          <div className="input-group justify-content-center">
+            <input
+              type="search"
+              className="word-input"
+              placeholder="Enter a word"
+              onChange={updateWord}
+            />
+            <button className="search-btn">Search</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
