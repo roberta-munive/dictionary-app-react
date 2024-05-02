2f46080807a24450f0aca8029b588aebfc45bea5
roberta-munive <114707010+roberta-munive@users.noreply.github.com>
2024-05-02 11:41:45 -0500

    Modified styling for Synonyms

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -4,14 +4,10 @@ import "./Synonyms.css";
 export default function Synonyms(props) {
   if (props.synonyms) {
     return (
-      <div className="Synonyms">
+      <div className="Synonyms synonyms-container">
         <span className="text-secondary me-3">Synonyms: </span>
         {props.synonyms.map(function (synonym, index) {
-          return (
-            <span key={index} className="me-4">
-              {synonym}
-            </span>
-          );
+          return <span key={index}>{synonym}</span>;
         })}{" "}
       </div>
     );
